d699732dceab2110bf8f4af191ee6295bf4ee989
mattraghu <mattraghu@aol.com>
2023-12-19 02:15:50 -0500

    Test Display and Clock Files

---
+++ Reciever/Reciever.srcs/sources_1/new/leddec.vhd
@@ -0,0 +1,58 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY leddec IS
+	PORT (
+		dig : IN STD_LOGIC_VECTOR (2 DOWNTO 0); -- which digit to currently display
+		data : IN STD_LOGIC_VECTOR (15 DOWNTO 0); -- 16-bit (4-digit) data
+		anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); -- which anode to turn on
+		seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)); -- segment code for current digit
+END leddec;
+
+ARCHITECTURE Behavioral OF leddec IS
+	SIGNAL data4 : STD_LOGIC_VECTOR (4 DOWNTO 0); -- binary value of current digit
+BEGIN
+	-- Select digit data to be displayed in this mpx period
+	data4(4 DOWNTO 1) <= data(3 DOWNTO 0) WHEN dig = "000" ELSE -- digit 0
+	         data(7 DOWNTO 4) WHEN dig = "001" ELSE -- digit 1
+	         data(11 DOWNTO 8) WHEN dig = "010" ELSE -- digit 2
+	         data(15 DOWNTO 12); -- digit 3
+
+
+	data4(0) <= 
+			'1' WHEN data(15 DOWNTO 12) = "0000" AND dig = "011" ELSE -- If the last digit is 0, turn off the last digit
+			'1' WHEN data(15 DOWNTO 8) = "00000000" AND dig = "010" ELSE -- If the last two digits are 0, turn off the last two digits
+			'1' WHEN data(15 DOWNTO 4) = "000000000000" AND dig = "001" ELSE -- If the last three digits are 0, turn off the last three digits
+			'0';	-- Otherwise, turn on the last digit
+
+
+
+	-- Turn on segments corresponding to 4-bit data word
+	seg <= "0000001" WHEN data4 = "00000" ELSE -- 0
+	       "1001111" WHEN data4 = "00010" ELSE -- 1
+	       "0010010" WHEN data4 = "00100" ELSE -- 2
+	       "0000110" WHEN data4 = "00110" ELSE -- 3
+	       "1001100" WHEN data4 = "01000" ELSE -- 4
+	       "0100100" WHEN data4 = "01010" ELSE -- 5
+	       "0100000" WHEN data4 = "01100" ELSE -- 6
+	       "0001111" WHEN data4 = "01110" ELSE -- 7
+	       "0000000" WHEN data4 = "10000" ELSE -- 8
+	       "0000100" WHEN data4 = "10010" ELSE -- 9
+	       "0001000" WHEN data4 = "10100" ELSE -- A
+	       "1100000" WHEN data4 = "10110" ELSE -- B
+	       "0110001" WHEN data4 = "11000" ELSE -- C
+	       "1000010" WHEN data4 = "11010" ELSE -- D
+	       "0110000" WHEN data4 = "11100" ELSE -- E
+	       "0111000" WHEN data4 = "11110" ELSE -- F
+	       "1111111";
+	-- Turn on anode of 7-segment display addressed by 3-bit digit selector dig
+	anode <= "11111110" WHEN dig = "000" ELSE -- 0
+	         "11111101" WHEN dig = "001" ELSE -- 1
+	         "11111011" WHEN dig = "010" ELSE -- 2
+	         "11110111" WHEN dig = "011" ELSE -- 3
+--	         "11101111" WHEN dig = "100" ELSE -- 4
+--	         "11011111" WHEN dig = "101" ELSE -- 5 
+--	         "10111111" WHEN dig = "110" ELSE -- 6
+--	         "01111111" WHEN dig = "111" ELSE -- 7
+	         "11111111";
+END Behavioral;
---
+++ Reciever/Reciever.srcs/sources_1/new/testLedec.vhd
@@ -0,0 +1,76 @@
+-- testLedec.vhd --
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY testLedec IS
+	PORT (
+		clk_100MHz : IN STD_LOGIC;
+		anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+		seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+	);
+END testLedec;
+
+ARCHITECTURE Behavioral OF testLedec IS
+
+	COMPONENT leddec IS
+		PORT (
+			dig : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+			data : IN STD_LOGIC_VECTOR (15 DOWNTO 0); -- DON'T change, data is fixed 4 bits in leddec for each displays
+			anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+			seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+		);
+	END COMPONENT;
+
+    SIGNAL S : STD_LOGIC_VECTOR (15 DOWNTO 0) := "0000000000000111"; -- Connect C1 and L1 for values of 4 digits (Default to 7)
+	SIGNAL md : STD_LOGIC_VECTOR (2 DOWNTO 0); -- mpx selects displays
+	SIGNAL display : STD_LOGIC_VECTOR (3 DOWNTO 0); -- Send digit for only one display to leddec
+    SIGNAL counter : STD_LOGIC_VECTOR (26 DOWNTO 0) := "000000000000000000000000000"; -- (2**27/(100E6)) = 1.34217728s for one cycle
+
+BEGIN
+	L1 : leddec
+	PORT MAP(dig => md, data => S, anode => anode, seg => seg);
+
+    -- Counter Process 
+    PROCESS(clk_100MHz)
+    BEGIN
+        IF (clk_100MHz'EVENT AND clk_100MHz = '1') THEN
+            IF (counter = "111111111111111111111111111") THEN
+                counter <= "000000000000000000000000000";
+            ELSE
+                counter <= counter + 1;
+            END IF;
+        END IF;
+    END PROCESS;
+
+    -- Process To Change Dig Every 1s
+    PROCESS(counter)
+    BEGIN
+        IF (counter = "111111111111111111111111111") THEN
+            md <= md + 1;
+        END IF;
+    END PROCESS;
+
+    
+
+	--mpx
+	--process(md)
+	--begin
+	-- if md = "00" then
+	-- display <= S(3 downto 0);
+	-- elsif md = "01" then
+	-- display <= S(7 downto 4);
+	-- elsif md = "10" then
+	-- display <= S(11 downto 8);
+	-- elsif md = "11" then
+	-- display <= S(15 downto 12);
+	-- end if;
+	--end process;
+
+	display <= S(3 DOWNTO 0) WHEN md = "000" ELSE
+	           S(7 DOWNTO 4) WHEN md = "001" ELSE
+	           S(11 DOWNTO 8) WHEN md = "010" ELSE
+	           S(15 DOWNTO 12);
+
+END Behavioral;
